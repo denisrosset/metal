7dd078e656ffc6f401ec299fed88c9dbe1eb7f90
Denis Rosset <physics@denisrosset.com>
2015-11-14 17:28:52 +0100

    SetOpsSuite cleanup

--- library/src/test/scala/metal/SetOpsSuite.scala
+++ library/src/test/scala/metal/SetOpsSuite.scala
@@ -6,14 +6,16 @@ import syntax._
 
 import spire.std.int._
 
-class SetOpsSuite extends FunSuite with BeforeAndAfter {
+class SetOpsSuite extends FunSuite {
 
+  // SearchableOps
   test("contains") {
     val sI = MHashSet(1, 2, 3, 4, 5)
     assert(sI.contains(3))
     assert(!sI.contains(6))
   }
 
+  // RemovableSearchableOps
   test("remove") {
     val sI = MHashSet(1, 2, 3, 4, 5)
     assert(sI.remove(2))
@@ -28,17 +30,19 @@ class SetOpsSuite extends FunSuite with BeforeAndAfter {
     assert(!sI.contains(2))
   }
 
+  // AddOps
+  test("+=") {
+    val sI = MHashSet(1, 2, 3)
+    assert((sI += 6) eq sI)
+    assert(sI.contains(6))
+  }
+
+  // SearchableAddOps
   test("add") {
     val sI = MHashSet(1, 2, 3)
     assert(sI.add(2))
     assert(!sI.add(6))
     assert(sI.contains(2))
-    assert(sI.contains(6))
-  }
-
-  test("+=") {
-    val sI = MHashSet(1, 2, 3)
-    assert((sI += 6) eq sI)
     assert(sI.contains(6))
   }
   
