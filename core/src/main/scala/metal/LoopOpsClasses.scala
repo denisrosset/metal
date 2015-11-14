70d19e8b2dc20c9e7c7ee76dfe7c6c833d47fd80
Denis Rosset <physics@denisrosset.com>
2015-11-14 17:28:37 +0100

    Renamed LoopClasses -> LoopOpsClasses for consistency

--- core/src/main/scala/metal/LoopOpsClasses.scala
+++ core/src/main/scala/metal/LoopOpsClasses.scala
@@ -27,8 +27,6 @@ final class LoopOpsE[E](val lhs: Enumerable with Elements[E]) {
 
 }
 
-
-
 final class LoopOpsK[K](val lhs: Enumerable with ElementsK[K]) {
 
   def foreach(body: K => Unit): Unit = macro macros.Loops.foreachK[K]
@@ -61,47 +59,34 @@ final class LoopOpsV[V](val lhs: Enumerable with ElementsV[V]) {
 
 }
 
-
 final class LoopOpsKV[K, V](val lhs: Enumerable with ElementsKV[K, V]) {
 
   def foreach(body: (K, V) => Unit): Unit = macro macros.Loops.foreachKV[K, V]
-  def foreach(body: ((K, V)) => Unit): Unit = macro macros.Loops.foreachE[(K, V)]
 
   def count(body: (K, V) => Boolean): Long = macro macros.Loops.countKV[K, V]
-  def count(body: ((K, V)) => Boolean): Long = macro macros.Loops.countE[(K, V)]
 
   def exists(body: (K, V) => Boolean): Boolean = macro macros.Loops.existsKV[K, V]
-  def exists(body: ((K, V)) => Boolean): Boolean = macro macros.Loops.existsE[(K, V)]
 
   def forall(body: (K, V) => Boolean): Boolean = macro macros.Loops.forallKV[K, V]
-  def forall(body: ((K, V)) => Boolean): Boolean = macro macros.Loops.forallE[(K, V)]
 
   def foldLeft[A](initialValue: A)(body: (A, K, V) => A): A = macro macros.Loops.foldLeftKV[A, K, V]
-  def foldLeft[A](initialValue: A)(body: (A, (K, V)) => A): A = macro macros.Loops.foldLeftE[A, (K, V)]
 
   def /:[A](initialValue: A)(body: (A, K, V) => A): A = macro macros.Loops.foldLeftKV[A, K, V]
-  def /:[A](initialValue: A)(body: (A, (K, V)) => A): A = macro macros.Loops.foldLeftE[A, (K, V)]
 
 }
 
 final class LoopOpsKV1V2[K, V1, V2](val lhs: Enumerable with ElementsKV1V2[K, V1, V2]) {
 
   def foreach(body: (K, V1, V2) => Unit): Unit = macro macros.Loops.foreachKV1V2[K, V1, V2]
-  def foreach(body: ((K, V1, V2)) => Unit): Unit = macro macros.Loops.foreachE[(K, V1, V2)]
 
   def count(body: (K, V1, V2) => Boolean): Long = macro macros.Loops.countKV1V2[K, V1, V2]
-  def count(body: ((K, V1, V2)) => Boolean): Long = macro macros.Loops.countE[(K, V1, V2)]
 
   def exists(body: (K, V1, V2) => Boolean): Boolean = macro macros.Loops.existsKV1V2[K, V1, V2]
-  def exists(body: ((K, V1, V2)) => Boolean): Boolean = macro macros.Loops.existsE[(K, V1, V2)]
 
   def forall(body: (K, V1, V2) => Boolean): Boolean = macro macros.Loops.forallKV1V2[K, V1, V2]
-  def forall(body: ((K, V1, V2)) => Boolean): Boolean = macro macros.Loops.forallE[(K, V1, V2)]
 
   def foldLeft[A](initialValue: A)(body: (A, K, V1, V2) => A): A = macro macros.Loops.foldLeftKV1V2[A, K, V1, V2]
-  def foldLeft[A](initialValue: A)(body: (A, (K, V1, V2)) => A): A = macro macros.Loops.foldLeftE[A, (K, V1, V2)]
 
   def /:[A](initialValue: A)(body: (A, K, V1, V2) => A): A = macro macros.Loops.foldLeftKV1V2[A, K, V1, V2]
-  def /:[A](initialValue: A)(body: (A, (K, V1, V2)) => A): A = macro macros.Loops.foldLeftE[A, (K, V1, V2)]
 
 }
